bcbd901d2100cedf602ca315451b75cf9a07aa1e
raj <rajmanishsahu1121@gmail.com>
2025-08-17 17:54:50 +0530

    crawl pages recursive working

--- crawl.js
+++ crawl.js
@@ -1,29 +1,56 @@
 const {JSDOM} = require('jsdom')
 
 
-async function crawlPage(currentURL) {
-    console.log(`actively crawling: ${currentURL}`)
+async function crawlPage(baseURL , currentURL, pages) {
 
-    try {
-    const resp = await fetch(currentURL)
-
-    if (resp.status > 399) {
-        console.log(`error in fetch with statuscode ${resp.status} on page ${currentURL}`)
-        return // just stop here (return) if the page in not available
-    }   
-    
-    // to ensure the resp is html
-    const contentType = resp.headers.get("content-type")
-    if (!contentType.includes("text/html")) {
-        console.log(`non html response , content type ${contentType} on page ${currentURL}`)
-        return // just stop here (return) if the resp is not a html 
-   
+    // to avoid crawling external urls on the page 
+    const baseURLObj = new URL(baseURL)
+    const currentURLObj = new URL(currentURL)
+    if(baseURLObj.hostname !== currentURLObj.hostname) {
+        return pages 
     }
 
-    console.log(await resp.text())
+    // check if that URL is already in your pages object:
+    // If it’s there (> 0), you increment it.
+    // If not, you add it to the object and set it to 1.
+    const normalizedCurrentURL = normalizeURL(currentURL)
+    if (pages[normalizedCurrentURL] > 0) {
+        pages[normalizedCurrentURL]++
+        return pages
+    }
+    pages[normalizedCurrentURL] = 1
+
+    console.log(`actively crawling: ${currentURL}`)
+
+
+    try {
+        const resp = await fetch(currentURL)
+
+        if (resp.status > 399) {
+            console.log(`error in fetch with statuscode ${resp.status} on page ${currentURL}`)
+            return pages // just stop here (return) if the page in not available and return pages
+        }   
+        
+        // to ensure the resp is html
+        const contentType = resp.headers.get("content-type")
+        if (!contentType.includes("text/html")) {
+            console.log(`non html response , content type ${contentType} on page ${currentURL}`)
+            return pages // just stop here (return) if the resp is not a html and return pages
+    
+        }
+
+        const htmlBody = await resp.text()
+
+        const nextURLs = getURLsFromHtml(htmlBody,baseURL)
+
+        for(const nextURL of nextURLs){
+            pages = await crawlPage(baseURL, nextURL , pages)
+        }
+
     } catch (err){
         console.log(`error in fetch ( ${err.message} ) , on page ${currentURL}`)
     }
+    return pages
 }
 
 function getURLsFromHtml(htmlBody, baseURL) {
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const {crawlPage} = require('./crawl.js')
 
-function main() {
+async function main() {
     if (process.argv.length < 3) {
         console.log("no website provided")
         process.exit(1)
@@ -12,7 +12,15 @@ function main() {
     const baseURL = process.argv[2]
     
     console.log(`starting crawl of ${baseURL}`)
-    crawlPage(baseURL)
+    const pages = await crawlPage(baseURL , baseURL , {})
+     // crawl pages return pages which is a promise not a object thats why await
+
+    // Object.entries(pages) this is because crawl pages return pages which is a promise not a object
+    // and we cant iterate over a promise 
+    // const page of pages is not valid 
+    for (const page of Object.entries(pages) ) {
+        console.log(page)
+    }
 }
 
 main()
