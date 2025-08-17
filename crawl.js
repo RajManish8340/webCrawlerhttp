fcbf0880c826564706fd5f29d8e9fc50f4bc698a
raj <rajmanishsahu1121@gmail.com>
2025-08-17 14:31:06 +0530

    getURLsFromHTML and tests

--- crawl.js
+++ crawl.js
@@ -1,4 +1,35 @@
-const { Uppercase, Lowercase } = require("@sinclair/typebox")
+const {JSDOM} = require('jsdom')
+
+function getURLsFromHtml(htmlBody, baseURL) {
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for (const linkElement of linkElements) {
+
+        if (linkElement.href.slice(0, 1) === '/') {
+            //relative
+            try{
+                const urlObj = new URL(`${baseURL}${linkElement.href}`)
+                urls.push(urlObj.href)
+                // because we are using URL contructor if url is valid we will push it 
+                //and if not we will cath the error and give a error message with that invalid url
+            } catch(err) {
+                console.log(`error with relative url : ${err.message}`)
+            }
+        } else {
+            //absolute
+            try{
+                const urlObj = new URL(linkElement.href)
+                urls.push(urlObj.href)
+                // because we are using URL contructor if url is valid we will push it 
+                //and if not we will cath the error and give a error message with that invalid url
+            } catch(err) {
+                console.log(`error with absolute url : ${err.message}`)
+            }
+        }
+    }
+    return urls
+}
 
 function normalizeURL(urlString){
     const urlObj = new URL(urlString)
@@ -12,5 +43,6 @@ function normalizeURL(urlString){
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL ,
+    getURLsFromHtml
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require('./crawl.js')
+const { normalizeURL ,getURLsFromHtml } = require('./crawl.js')
 const { test, expect } = require('@jest/globals')
 
 test('normalizeURL strip protocol ', () => {
@@ -28,5 +28,79 @@ test('normalizeURL strip http ', () => {
     const input = 'http://blog.boot.dev/path'
     const actual = normalizeURL(input)
     const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHtml absolute', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href="https://blog.boot.dev/path">
+            Boot.dev Blog
+        </a>
+    <body>
+<html>
+`
+    const inputBaseURL = "https://blog.boot.dev"
+    const actual = getURLsFromHtml(inputHTMLBody,inputBaseURL)
+    /* The href attribute is just a string. But when you access it in JavaScript via linkElement.href, the browser resolves it into a fully qualified URL using the WHATWG URL standard.
+In that standard:
+https://blog.boot.dev → missing a path → normalized → https://blog.boot.dev/
+
+https://blog.boot.dev/path → already has a path → unchanged*/
+    const expected = ["https://blog.boot.dev/path"] 
+    expect(actual).toEqual(expected)
+})
+
+
+
+test('getURLsFromHtml relative', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href="/path/">
+            Boot.dev Blog
+        </a>
+    <body>
+<html>
+`
+    const inputBaseURL = "https://blog.boot.dev"
+    const actual = getURLsFromHtml(inputHTMLBody,inputBaseURL)
+    const expected = ["https://blog.boot.dev/path/"] 
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHtml both absolute and relative', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href="https://blog.boot.dev/path1/">
+            Boot.dev Blog Path One
+        </a>
+        <a href="/path2/">
+            Boot.dev Blog Path Two 
+        </a>
+    <body>
+<html>
+`
+    const inputBaseURL = "https://blog.boot.dev"
+    const actual = getURLsFromHtml(inputHTMLBody,inputBaseURL)
+    const expected = ["https://blog.boot.dev/path1/", "https://blog.boot.dev/path2/"] 
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHtml invalid', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href="invalid">
+            Boot.dev Blog
+        </a>
+    <body>
+<html>
+`
+    const inputBaseURL = "https://blog.boot.dev"
+    const actual = getURLsFromHtml(inputHTMLBody,inputBaseURL)
+    const expected = [] 
     expect(actual).toEqual(expected)
 })
